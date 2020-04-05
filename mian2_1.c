d29adccbeaf507f61bd19df22a2d7b3020c13321
Jordan <jordantangy@gmail.com>
2020-04-05 23:35:34 +0300

    new version

--- mian2_1.c
+++ mian2_1.c
@@ -1,28 +1,29 @@
 #include <sys/types.h>
 #include <stdio.h>
+#include <unistd.h>
 
 
 int main(){
     pid_t pid = fork();
-    if (pid == 0){ //childe
+    if (pid == 0){ //child
         pid_t pid2 = fork();
-        if (pid2 == 0){ //childe childe
+        if (pid2 == 0){ //childe child
             for (size_t i = 0; i < 60; i++){
-                printf("I'm childe's childe");
-                usleep(1000000);
+                printf("I'm child's child\n");
+                usleep(100);
             }
         }
-        else{ // childe
+        else{ // child
             for (size_t i = 0; i < 60; i++){
-                printf("I'm childe");
-                usleep(1000000);
+                printf("I'm child\n");
+                usleep(100);
             }
         }
     }
     else{ // parent
         for (size_t i = 0; i < 60; i++){
-            printf("I'm parent");
-            usleep(1000000);
+            printf("I'm parent\n");
+            usleep(100);
         }
         
     }
